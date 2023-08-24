26b196a54146095745d97fb8ccc8203c827b5cec
Tar0k <73.tarok@gmail.com>
2023-08-24 18:19:45 +0400

    added delete, edit functions for phonebook

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import show_contacts, generate_contacts, delete_contacts, AddContactView
+from .views import show_contacts, generate_contacts, delete_contacts, AddContactView, EditContactView, RemoveContactView
 
 urlpatterns = [
     path('', show_contacts, name="show_contacts"),
     path('generate_contacts/<int:times>', generate_contacts, name="generate_contacts"),
     path('delete_contacts/', delete_contacts, name="delete_contacts"),
     path('create_contact/', AddContactView.as_view(), name="create_contact"),
+    path('edit_contact/<int:pk>', EditContactView.as_view(), name="edit_contact"),
+    path("remove_contact/<int:pk>", RemoveContactView.as_view(), name="remove_contact"),
 ]
--- phonebook/views.py
+++ phonebook/views.py
@@ -2,11 +2,12 @@ import random
 from django.shortcuts import render
 from django.http import HttpResponseBadRequest
 from django.shortcuts import redirect
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from faker import Faker
 from .models import Contacts
+
 # Create your views here.
 
 fake = Faker("ru-Ru")
@@ -19,9 +20,22 @@ def show_contacts(request):
 
 
 class AddContactView(CreateView):
+    template_name = 'phonebook/create_contact.html'
     model = Contacts
     fields = ['first_name', 'last_name', 'middle_name', 'phone_number']
-    template_name = 'phonebook/create_contact.html'
+    success_url = reverse_lazy(show_contacts)
+
+
+class EditContactView(UpdateView):
+    template_name = 'phonebook/edit_contact.html'
+    model = Contacts
+    fields = ['first_name', 'last_name', 'middle_name', 'phone_number']
+    success_url = reverse_lazy(show_contacts)
+
+
+class RemoveContactView(DeleteView):
+    template_name = 'phonebook/remove_contact.html'
+    model = Contacts
     success_url = reverse_lazy(show_contacts)
 
 
@@ -47,10 +61,12 @@ def generate_contacts(request, times):
     for _ in range(times):
         if contacts_quantity < 100:
             new_person = create_random_person()
-            Contacts.objects.create(first_name=new_person['first_name'],
-                                    last_name=new_person['last_name'],
-                                    middle_name=new_person['middle_name'],
-                                    phone_number=new_person['phone_number'])
+            result = Contacts.objects.create(first_name=new_person['first_name'],
+                                             last_name=new_person['last_name'],
+                                             middle_name=new_person['middle_name'],
+                                             phone_number=new_person['phone_number'])
+            if result is not None:
+                contacts_quantity += 1
 
     return redirect(show_contacts)
 
