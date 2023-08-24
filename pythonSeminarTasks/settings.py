7a034f7ee9f2d76f60386ea855d7e7053e997631
Tar0k <73.tarok@gmail.com>
2023-08-24 19:00:17 +0400

    security_fix1

--- pythonSeminarTasks/settings.py
+++ pythonSeminarTasks/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("/etc/secret_key.txt") as f:
+with open(Path(BASE_DIR, 'etc', 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['http://tarok.pythonanywhere.com']
+ALLOWED_HOSTS = ['tarok.pythonanywhere.com']
 
 
 # Application definition
