0c22bba63c5b6684ebcb1e9b38a10515b548cc90
Tar0k <73.tarok@gmail.com>
2023-08-25 20:50:39 +0400

    bootstrapped again

--- pythonSeminarTasks/settings.py
+++ pythonSeminarTasks/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+if not DEBUG:
+    STATIC_ROOT = "/home/Tarok/pythonSeminarTasks/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
