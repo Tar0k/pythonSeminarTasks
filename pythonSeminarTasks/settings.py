b4ef5d94cdf5c79883cb092cfa3426b14cc0eba5
Tar0k <73.tarok@gmail.com>
2023-08-25 20:53:45 +0400

    fix static

--- pythonSeminarTasks/settings.py
+++ pythonSeminarTasks/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 if not DEBUG:
     STATIC_ROOT = "/home/Tarok/pythonSeminarTasks/static"
 
