c62176c30d76225bd162cfb11bb832df54fee705
Tar0k <73.tarok@gmail.com>
2023-08-25 20:51:49 +0400

    debug off

--- pythonSeminarTasks/settings.py
+++ pythonSeminarTasks/settings.py
@@ -24,7 +24,7 @@ with open(Path(BASE_DIR, 'etc', 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = []
